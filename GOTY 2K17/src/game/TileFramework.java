bd3ad6e8f7ddfada68476bcbd6782c48ae969f45
Nick Hippen <nhippen973@gmail.com>
2017-04-23 21:26:49 -0500

    Removed remaining excess code from SpriteGame

--- GOTY 2K17/src/game/TileFramework.java
+++ GOTY 2K17/src/game/TileFramework.java
@@ -1,22 +1,15 @@
 package game;
 
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import game.maps.Map2;
 import game.sprites.BoundingSprite;
-import game.sprites.SpellSprite;
 import game.units.Unit;
-import game.units.monsters.Zombie;
 import game.util.Matrix3x3f;
 import game.util.SimpleFramework;
-import game.vectors.Vector2f;
 import game.vectors.VectorObject;
 
 
@@ -30,10 +23,6 @@ public class TileFramework extends SimpleFramework {
 
 	public static final float TILE_SIZE_X = APP_WORLD_WIDTH / (float) TILES_X;
 	public static final float TILE_SIZE_Y = APP_WORLD_HEIGHT / (float) TILES_Y;
-	
-	private BoundingSprite actor;
-	private List<BoundingSprite> boundingSprites;
-	private List<SpellSprite> spellSprites;
 	
 	boolean displayBounds;
 	
@@ -54,114 +43,17 @@ public class TileFramework extends SimpleFramework {
 	protected void initialize() {
 		super.initialize();
 		tileWorld = new TileWorld(new Map2(), TILES_X, TILES_Y, TILE_SIZE_X, TILE_SIZE_Y, APP_WORLD_WIDTH, APP_WORLD_HEIGHT);
-		
-		boundingSprites = new ArrayList<>();
-		spellSprites = new ArrayList<>();
-		
-		// Actor
-		actor = new Zombie();
-		actor.setLocation(new Vector2f(0, 0));
 	}
 	
 	@Override
 	protected void processInput(float delta) {
 		super.processInput(delta);
-		// Movement
-		boolean walking = false;
-		if (keyboard.keyDown(KeyEvent.VK_W)) {
-			actor.move(new Vector2f(0F, 0.2F), boundingSprites, delta);
-//			actor.setDirection(Direction.NORTH);
-			walking = true;
-		} else if (keyboard.keyDown(KeyEvent.VK_S)) {
-			actor.move(new Vector2f(0F, -0.2F), boundingSprites, delta);
-//			actor.setDirection(Direction.SOUTH);
-			walking = true;
-		} else if (keyboard.keyDown(KeyEvent.VK_A)) {
-			actor.move(new Vector2f(-0.2F, 0F), boundingSprites, delta);
-//			actor.setDirection(Direction.WEST);
-			walking = true;
-		} else if (keyboard.keyDown(KeyEvent.VK_D)) {
-			actor.move(new Vector2f(0.2F, 0F), boundingSprites, delta);
-//			actor.setDirection(Direction.EAST);
-			walking = true;
-		}
-		
-		// Reset
-		if (keyboard.keyDownOnce(KeyEvent.VK_R)) {
-//			actor.setDead(false);
-			actor.setLocation(new Vector2f());
-			actor.setRotation(0F);
-//			actor.setDirection(Direction.SOUTH);
-		}
-		
-		// Spawn Spell
-		if (keyboard.keyDownOnce(KeyEvent.VK_SPACE)) {
-			SpellSprite spell = new SpellSprite();
-			spell.setLocation(new Vector2f(-1.1F, 0F));
-			spellSprites.add(spell);
-		}
-		
 		// Display Bounds
 		if (keyboard.keyDownOnce(KeyEvent.VK_B)) {
 			displayBounds = !displayBounds;
 		}
-//		actor.setWalking(walking);
-//		actor.updateAnimations(delta);
 	}
 
-	@Override
-	protected void updateObjects(float delta) {
-		super.updateObjects(delta);
-		
-		// Move spells
-		for (int i = spellSprites.size() - 1; i >= 0; i--) {
-			SpellSprite sprite = spellSprites.get(i);
-			sprite.move(new Vector2f(0.4F, 0), new ArrayList<>(), delta);
-			if (sprite.getLocation().x > 1.5F
-					|| sprite.getLocation().y > 1.5F
-					|| sprite.getLocation().y < -0.5F) {
-				spellSprites.remove(i);
-			}
-			sprite.setRotation(sprite.getRotation() + 4F * delta);
-			if (sprite.hasAnyBoundViolations(Arrays.asList(actor))) {
-				spellSprites.remove(i);
-//				actor.setDead(true);
-			}
-		}
-	}
-
-	@Override
-	protected void render(Graphics g) {
-		super.render(g);
-		Graphics2D g2d = (Graphics2D) g;
-		Matrix3x3f view = getViewportTransform();
-		
-		// Actor
-		actor.setView(view);
-		actor.draw(g2d);
-		if (displayBounds) {
-			renderBounds(actor, g2d);
-		}
-
-		// Objects
-		for (BoundingSprite sprite : boundingSprites) {
-			sprite.setView(view);
-			sprite.draw(g2d);
-			if (displayBounds) {
-				renderBounds(sprite, g2d);
-			}
-		}
-		
-		// Spells
-		for (SpellSprite sprite : spellSprites) {
-			sprite.setView(view);
-			sprite.draw(g2d);
-			if (displayBounds) {
-				renderBounds(sprite, g2d);
-			}
-		}
-	}
-	
 	public void renderTiles(Graphics2D g2d) {
 		Matrix3x3f view = getViewportTransform();
 		for (Tile[] tiles : tileWorld.getTiles()) {
