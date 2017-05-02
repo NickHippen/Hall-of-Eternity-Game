1618048317d56bf1767c54c025b8cadf62c6f7f1
Kevin Chen <kchen@unomaha.edu>
2017-05-02 16:01:56 -0500

    Added other mobs in MonsterType enum

--- GOTY 2K17/src/game/units/monsters/MonsterType.java
+++ GOTY 2K17/src/game/units/monsters/MonsterType.java
@@ -3,6 +3,13 @@ package game.units.monsters;
 public enum MonsterType {
 
 	ZOMBIE,
-	DRAGON;
+	SWORD_SKELETON,
+	BOW_SKELETON,
+	SPIDER,WOLF,
+	CYCLOPS,
+	CENTAUR,
+	CERBERUS,
+	DRAGON,
+	ZOMBIE_DRAGON;
 	
 }
