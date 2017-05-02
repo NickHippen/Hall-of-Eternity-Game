e545bfa03bc32f84a16e70311a422205f6f9e971
NICK <nrichman962@gmail.com>
2017-05-01 21:28:05 -0500

    Title screen and menus

--- GOTY 2K17/src/game/Game.java
+++ GOTY 2K17/src/game/Game.java
@@ -20,225 +20,264 @@ import game.units.heroes.Freelancer;
 import game.units.monsters.Boss;
 import game.util.Matrix3x3f;
 import game.vectors.Vector2f;
+import menu.TitleScreen;
 
 public class Game extends TileFramework {
 
 	private static final Random RANDOM = new Random();
-	
+
 	private Deck deck;
 	private Card grabbedCard;
 	private Card activatedCard;
-	
+
 	private int waveNum;
 	private int boneNum;
-	Vector2f centeredMouseVec;
-	
+	Vector2f mouseVec;
+
 	boolean selectingTarget;
 	boolean selectingArea;
-	
+
 	ArrayList<Unit> selectedUnits;
-	//Used for card movement
+	// Used for card movement
 	Vector2f initialLoc = null;
 	Vector2f initialLocCard = null;
-	
+
 	private String message;
 
+	private boolean titleScreen;
+	private boolean levelSelection;
+	private boolean deckCreation;
+	private boolean gameplay;
+
+	private TitleScreen title;
+	
 	@Override
 	protected void initialize() {
 		super.initialize();
-		
+
 		deck = new Deck(getWorld());
-		
+
 		getWorld().policyIteration(Tile::getStandardPathfinding);
 		getWorld().policyIteration(Tile::getAggroPathfinding);
-		
+
 		this.waveNum = 0;
 		this.boneNum = 123;
 		this.message = "";
-		
+
 		selectedUnits = new ArrayList<Unit>();
+
+		this.titleScreen = true;
+		
+		title = new TitleScreen(getWorld());
 	}
-	
+
 	@Override
 	protected void processInput(float delta) {
 		super.processInput(delta);
-		
-		centeredMouseVec = getCenteredRelativeWorldMousePosition();
-		
-		// Player drops card
-		if (!mouse.buttonDown(MouseEvent.BUTTON1) && grabbedCard != null) {
-			if(onBoard(centeredMouseVec)){
-				if(grabbedCard instanceof MonsterSpawnCard) selectingTarget = true;
-				if(grabbedCard instanceof AreaCard) selectingArea = true;
-				deck.setRemoved(deck.getHand().indexOf(grabbedCard));
-				deck.getHand().remove(grabbedCard);
-				activatedCard = grabbedCard;
-				for (Unit unit : getWorld().getUnits()) {
-					if (unit instanceof Boss) {
-						((Boss) unit).setAttacking(true);
-						break;
-					}
+		mouseVec = getCenteredRelativeWorldMousePosition();
+
+		if (titleScreen) {
+			if (mouseVec.x < .55 && mouseVec.x > -.55 && mouseVec.y < -1.2 && mouseVec.x > -1.57){
+				if(mouse.buttonDownOnce(MouseEvent.BUTTON1)){
+					titleScreen = false;
+					gameplay = true;
 				}
-			}else{ 
-				//Puts card back in its original spot
-				deck.getHand().get(deck.getHand().indexOf(grabbedCard)).setLocation(new Vector2f(-2.95f + ((deck.getHand().indexOf(grabbedCard)) * 1f), -0.87f));
-			}
-			grabbedCard = null;
+				title.selectButton();
+			}else title.unselectButton();
 		}
 
-		//Player grabs card
-		if(!selectingTarget && !onBoard(centeredMouseVec)){
-			if (mouse.buttonDownOnce(MouseEvent.BUTTON1)) {
-				for (Card card : deck.getHand()) {
-					if (card.isPointWithin(centeredMouseVec)) {
-						grabbedCard = card;
-						initialLocCard = grabbedCard.getLocation();
-						initialLoc = centeredMouseVec;
-						break;
+		if (gameplay) {
+			// Player drops card
+			if (!mouse.buttonDown(MouseEvent.BUTTON1) && grabbedCard != null) {
+				if (onBoard(mouseVec)) {
+					if (grabbedCard instanceof MonsterSpawnCard)
+						selectingTarget = true;
+					if (grabbedCard instanceof AreaCard)
+						selectingArea = true;
+					deck.setRemoved(deck.getHand().indexOf(grabbedCard));
+					deck.getHand().remove(grabbedCard);
+					activatedCard = grabbedCard;
+					for (Unit unit : getWorld().getUnits()) {
+						if (unit instanceof Boss) {
+							((Boss) unit).setAttacking(true);
+							break;
+						}
+					}
+				} else {
+					// Puts card back in its original spot
+					deck.getHand().get(deck.getHand().indexOf(grabbedCard))
+							.setLocation(new Vector2f(-2.95f + ((deck.getHand().indexOf(grabbedCard)) * 1f), -0.87f));
+				}
+				grabbedCard = null;
+			}
+
+			// Player grabs card
+			if (!selectingTarget && !onBoard(mouseVec)) {
+				if (mouse.buttonDownOnce(MouseEvent.BUTTON1)) {
+					for (Card card : deck.getHand()) {
+						if (card.isPointWithin(mouseVec)) {
+							grabbedCard = card;
+							initialLocCard = grabbedCard.getLocation();
+							initialLoc = mouseVec;
+							break;
+						}
 					}
 				}
 			}
-		}
-		
-		//Initial code for unit selection
-		if(onBoard(centeredMouseVec)){
-			if(mouse.buttonDownOnce(MouseEvent.BUTTON1)){
-				selectedUnits.clear();
-				int xPos = getWorld().getTileLocationAtPosition(centeredMouseVec).getX();
-				int yPos = getWorld().getTileLocationAtPosition(centeredMouseVec).getY();
-				selectedUnits.addAll((getWorld().getTiles()[xPos][yPos].getUnits()));
-				for(int i = 0; i < selectedUnits.size(); i++){
-					//Do something here on unit select
+
+			// Initial code for unit selection
+			if (onBoard(mouseVec)) {
+				if (mouse.buttonDownOnce(MouseEvent.BUTTON1)) {
+					selectedUnits.clear();
+					int xPos = getWorld().getTileLocationAtPosition(mouseVec).getX();
+					int yPos = getWorld().getTileLocationAtPosition(mouseVec).getY();
+					selectedUnits.addAll((getWorld().getTiles()[xPos][yPos].getUnits()));
+					for (int i = 0; i < selectedUnits.size(); i++) {
+						// Do something here on unit select
+					}
 				}
 			}
-		}
-		
-		//Moves card that player has grabbed
-		if (mouse.buttonDown(MouseEvent.BUTTON1)) {
-			if (grabbedCard != null) {
-				grabbedCard.setLocation(initialLocCard.sub(initialLoc.sub(centeredMouseVec)));
+
+			// Moves card that player has grabbed
+			if (mouse.buttonDown(MouseEvent.BUTTON1)) {
+				if (grabbedCard != null) {
+					grabbedCard.setLocation(initialLocCard.sub(initialLoc.sub(mouseVec)));
+				}
 			}
-		}
-		
-		//Draw card by clicking on deck
-		if(deck.getCardBack().isPointWithin(centeredMouseVec)){
-			if (mouse.buttonDownOnce(MouseEvent.BUTTON1)){
-				deck.drawCard();
+
+			// Draw card by clicking on deck
+			if (deck.getCardBack().isPointWithin(mouseVec)) {
+				if (mouse.buttonDownOnce(MouseEvent.BUTTON1)) {
+					deck.drawCard();
+				}
 			}
-		}
-	
-		
-		//Player selects single tile
-		if(mouse.buttonDownOnce(MouseEvent.BUTTON1) && selectingTarget){
-			if (activatedCard.performAction(centeredMouseVec)) {
-				selectingTarget = false;
-				grabbedCard = null;
+
+			// Player selects single tile
+			if (mouse.buttonDownOnce(MouseEvent.BUTTON1) && selectingTarget) {
+				if (activatedCard.performAction(mouseVec)) {
+					selectingTarget = false;
+					grabbedCard = null;
+				}
 			}
-		}
-		
-		//Player selects 3x3 area tile
-		if(mouse.buttonDownOnce(MouseEvent.BUTTON1) && selectingArea){
-			if (activatedCard.performAction(centeredMouseVec)) {
-				selectingArea = false;
-				grabbedCard = null;
+
+			// Player selects 3x3 area tile
+			if (mouse.buttonDownOnce(MouseEvent.BUTTON1) && selectingArea) {
+				if (activatedCard.performAction(mouseVec)) {
+					selectingArea = false;
+					grabbedCard = null;
+				}
 			}
-		}
-		
-		if (keyboard.keyDownOnce(KeyEvent.VK_SPACE)) {
-			getWorld().addUnitToTile(new TileLocation(0, RANDOM.nextInt(4) + 5), new Freelancer(getWorld()));
+
+			if (keyboard.keyDownOnce(KeyEvent.VK_SPACE)) {
+				getWorld().addUnitToTile(new TileLocation(0, RANDOM.nextInt(4) + 5), new Freelancer(getWorld()));
+			}
 		}
 	}
-	
+
 	@Override
 	protected void updateObjects(float delta) {
 		super.updateObjects(delta);
-		
-		deck.getCardBack().setLocation(new Vector2f(-2.95f + 5f, -0.87f));
 
-		deck.drawAnimation(delta);
-	
-		List<Unit> units = getWorld().getUnits();
-		for (int i = units.size() - 1; i >= 0; i--) {
-			Unit unit = units.get(i);
-			unit.update(delta);
+		if (gameplay) {
+			deck.getCardBack().setLocation(new Vector2f(-2.95f + 5f, -0.87f));
+
+			deck.drawAnimation(delta);
+
+			List<Unit> units = getWorld().getUnits();
+			for (int i = units.size() - 1; i >= 0; i--) {
+				Unit unit = units.get(i);
+				unit.update(delta);
+			}
 		}
 	}
-	
+
 	@Override
 	protected void render(Graphics g) {
-		super.render(g);		
+		super.render(g);
 
 		Matrix3x3f view = getViewportTransform();
 		Graphics2D g2d = (Graphics2D) g;
-		
-		getWorld().getMap().setView(view);
-		getWorld().getMap().draw(g2d);
 
-		renderTiles(g2d);
-		
-		if (selectingTarget || selectingArea || displayCoordinates) {
-			renderGrid(g2d);
-		}
-		if (displayDirections != 0) {
-			renderDirections(g2d);
-		}
-		if (displayCoordinates) {
-			renderCoordinates(g2d);
+		if(titleScreen){
+			title.setView(view);
+			title.draw(g2d);
 		}
 		
-		//Renders Wave and Bones text
-		g.setColor(Color.WHITE);
-		g.setFont(new Font("Cooper Black", Font.PLAIN, 18));
-		g.drawString("WAVE", 90,640);
-		g.drawString("BONES", 1287, 640);
-		
-		if(selectingTarget || selectingArea){
-			g.setFont(new Font("Cooper Black", Font.PLAIN, 20));
-			message = "";
-			message = "Activated";
-			g.drawString(message, 581, 645);
-			g.setColor(Color.ORANGE);
-			message = activatedCard.getName();
-			g.drawString(message, 688, 645);
+		if (gameplay) {
+			getWorld().getMap().setView(view);
+			getWorld().getMap().draw(g2d);
+
+			renderTiles(g2d);
+
+			if (selectingTarget || selectingArea || displayCoordinates) {
+				renderGrid(g2d);
+			}
+			if (displayDirections != 0) {
+				renderDirections(g2d);
+			}
+			if (displayCoordinates) {
+				renderCoordinates(g2d);
+			}
+
+			// Renders Wave and Bones text
 			g.setColor(Color.WHITE);
-			if(activatedCard instanceof CurseCard) message = "select target location";
-			if(activatedCard instanceof MonsterSpawnCard) message = "select summon location";
-			g.drawString(message, 581, 666);
-			message = "";
-		}
-		
-		
-		g.setFont(new Font("Titillium Web", Font.PLAIN, 25));
-		g.drawString(String.format("%03d", waveNum), 99, 664);
-		g.drawString(String.format("%03d", boneNum), 1299, 664);
-	
-		
-		//Renders map tile selection or area selection
-		if ((selectingArea && grabbedCard == null) && onBoard(centeredMouseVec)) renderSelectedArea(g2d, getWorld().getTileLocationAtPosition(centeredMouseVec));
-		else if ((selectingTarget || grabbedCard == null) && onBoard(centeredMouseVec)) renderSelectedTile(g2d, getWorld().getTileLocationAtPosition(centeredMouseVec));
+			g.setFont(new Font("Cooper Black", Font.PLAIN, 18));
+			g.drawString("WAVE", 90, 640);
+			g.drawString("BONES", 1287, 640);
 
-		// Renders cards
-		for (int i = 0; i < deck.getHand().size(); i++) {
-			Card card = deck.getHand().get(i);
-			card.setView(view);
-			card.draw(g2d);
-			//Shows line around selected cards
-			g.setColor(Color.green);
-			if (card.isPointWithin(centeredMouseVec) && !selectingTarget && grabbedCard == null) card.getOuterBound().render(g);
-		}
-		
-		//Renders the card back
-		deck.getCardBack().setView(view);
-		if(deck.getCardsRemaining() > 0) deck.getCardBack().draw(g2d);
-		
-		//Render grabbed card last so it's on top of everything
-		if(grabbedCard != null) grabbedCard.draw(g2d);
-		if(grabbedCard != null) grabbedCard.getOuterBound().render(g);
+			if (selectingTarget || selectingArea) {
+				g.setFont(new Font("Cooper Black", Font.PLAIN, 20));
+				message = "";
+				message = "Activated";
+				g.drawString(message, 581, 645);
+				g.setColor(Color.ORANGE);
+				message = activatedCard.getName();
+				g.drawString(message, 688, 645);
+				g.setColor(Color.WHITE);
+				if (activatedCard instanceof CurseCard)
+					message = "select target location";
+				if (activatedCard instanceof MonsterSpawnCard)
+					message = "select summon location";
+				g.drawString(message, 581, 666);
+				message = "";
+			}
 
+			g.setFont(new Font("Titillium Web", Font.PLAIN, 25));
+			g.drawString(String.format("%03d", waveNum), 99, 664);
+			g.drawString(String.format("%03d", boneNum), 1299, 664);
+
+			// Renders map tile selection or area selection
+			if ((selectingArea && grabbedCard == null) && onBoard(mouseVec))
+				renderSelectedArea(g2d, getWorld().getTileLocationAtPosition(mouseVec));
+			else if ((selectingTarget || grabbedCard == null) && onBoard(mouseVec))
+				renderSelectedTile(g2d, getWorld().getTileLocationAtPosition(mouseVec));
+
+			// Renders cards
+			for (int i = 0; i < deck.getHand().size(); i++) {
+				Card card = deck.getHand().get(i);
+				card.setView(view);
+				card.draw(g2d);
+				// Shows line around selected cards
+				g.setColor(Color.green);
+				if (card.isPointWithin(mouseVec) && !selectingTarget && grabbedCard == null)
+					card.getOuterBound().render(g);
+			}
+
+			// Renders the card back
+			deck.getCardBack().setView(view);
+			if (deck.getCardsRemaining() > 0)
+				deck.getCardBack().draw(g2d);
+
+			// Render grabbed card last so it's on top of everything
+			if (grabbedCard != null)
+				grabbedCard.draw(g2d);
+			if (grabbedCard != null)
+				grabbedCard.getOuterBound().render(g);
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		launchApp(new Game());
 	}
-	
+
 }
---
+++ GOTY 2K17/src/menu/TitleScreen.java
@@ -0,0 +1,53 @@
+package menu;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import game.TileFramework;
+import game.TileWorld;
+import game.sprites.BoundingSprite;
+import game.units.monsters.Zombie;
+import game.vectors.AxisAlignedBoundingBox;
+import game.vectors.Vector2f;
+
+public class TitleScreen extends BoundingSprite{
+
+	private static BufferedImage spriteSheet;
+	
+	static {
+		try {
+			URL url = Zombie.class.getResource("/resources/menus/title.png");
+			spriteSheet = ImageIO.read(url);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public TitleScreen(TileWorld world) {
+		super(spriteSheet, 1, 1);		
+		
+		setOuterBound(new AxisAlignedBoundingBox(
+				new Vector2f(-TileFramework.TILE_SIZE_X / 2F, TileFramework.TILE_SIZE_Y / 2F),
+				new Vector2f(TileFramework.TILE_SIZE_X / 2F, -TileFramework.TILE_SIZE_Y / 2F)));
+	}
+	
+	public void selectButton(){
+		try {
+			URL url = Zombie.class.getResource("/resources/menus/select_title.png");
+			BufferedImage spriteSheet2 = ImageIO.read(url);
+			this.setRenderedImage(spriteSheet2);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public void unselectButton(){
+			this.setRenderedImage(spriteSheet);
+	}
+	
+}
